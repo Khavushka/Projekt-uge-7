6488d8e460d6d5e3fd5e6912c2a85d55d404f58a
mrBanden <martinhaaningan@gmail.com>
2021-02-18 15:00:53 +0100

    læs xml med fs

--- controllers/handlersBooks.js
+++ controllers/handlersBooks.js
@@ -5,15 +5,30 @@
  * About Contacts
  */
 const fs = require("fs"); 
+const FILENAME = __dirname + '../public/booksCanon.xml';
+const convert = require("xml-js");
 const httpStatus = require("http-status-codes");
 const lib = require("./utilities/libWebUtil");          // home grown utilities
 const tmpl = require("../views/myTemplater");            // highly experimental template
 const mod = require("../models/jsonBooks");          // models
 
 module.exports = {
-    updBooks (req, res, data) {
-        let obj = lib.makeWebArrays(req, data);         // home made GET and POST objects
-        console.log(`${obj.POST.title} ${obj.POST.publisher}`); 
+
+    async updBooks (req, res, data) {
+        let obj = lib.makeWebArrays(req, data);         // home made GET and POST object
+        
+        await fs.readFile(FILENAME, 'utf-8', function(err, data) {
+            if (err) {
+                console.log("error loading file");
+            } else {
+                let booksjson = convert.xml2json(data, {compact: true, spaces: 4});
+                console.log(booksjson);
+            }
+        });
+
+        //let booksjson = convert.xml2json(xmlstring, {compact: true, spaces: 4});//
+        
+
         res.writeHead(httpStatus.MOVED_PERMANENTLY, {"Location": "htttp://localhost:3000/books"});
         res.end();
     },
